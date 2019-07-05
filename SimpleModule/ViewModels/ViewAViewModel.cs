bb7457e8dc62ca7c4341ca581e473897a4b15228
Bek <Bek.Go@outlook.com>
2019-07-05 17:07:59 +0500

    Add project files.

---
+++ SimpleModule/ViewModels/ViewAViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Prism.Regions;
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleModule.ViewModels
+{
+    public class ViewAViewModel : ReactiveObject, INavigationAware
+    {
+
+        public bool IsNavigationTarget(NavigationContext navigationContext)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnNavigatedFrom(NavigationContext navigationContext)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnNavigatedTo(NavigationContext navigationContext)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
